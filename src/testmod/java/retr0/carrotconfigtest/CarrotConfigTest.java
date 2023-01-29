75b5944600ef6cd7fedf3533a1af04b9096f0cce
Retr0 <18603664+2Retr0@users.noreply.github.com>
2023-01-29 13:35:05 -0800

    Add test mod and bump Minecraft version to 1.19.3

--- src/testmod/java/retr0/carrotconfigtest/CarrotConfigTest.java
+++ src/testmod/java/retr0/carrotconfigtest/CarrotConfigTest.java
@@ -1,21 +1,23 @@
-package net.fabricmc.example;
+package retr0.carrotconfigtest;
 
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+public class CarrotConfigTest implements ClientModInitializer {
+	public static final String MOD_ID = "carrotconfigtest";
+
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
-	public void onInitialize() {
+	public void onInitializeClient() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Hello CarrotConfig Test!");
 	}
 }
